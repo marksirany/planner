8ec689585832dc85723fadc288d1bc3a66b2d540
Mark Sirany <marksirany@me.com>
2019-11-21 10:52:59 -0600

    created time, input, and save button in html

---
+++ script.js
@@ -0,0 +1,8 @@
+//show current date//
+//You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.//
+
+
+//Clicking on the save button will store the time and user input in `localStorage`//
+
+
+//each hour should be color coded to reflect whether the time slot is in the past, the present, or the future. This will change depending on the time of day//
